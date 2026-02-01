cb8e3b78adfd0fefce8dd8183c8151317100282f
ych3r <liuyc26@gmail.com>
2026-01-31 19:22:34 -0500

    update orm

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,28 +1,10 @@
 import uvicorn
 from typing import Annotated
-from fastapi import Depends, FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
-from sqlmodel import Session, SQLModel, create_engine, select
+from sqlmodel import Session, select
 from app.models import Target, TargetCreate, TargetUpdate
-
-
-# Create an engine
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
-
-# Create the tables
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
-# Create a session dependency
-def get_session():
-    with Session(engine) as session:
-        yield session
-
-SessionDep = Annotated[Session, Depends(get_session)]
+from app.database import create_db_and_tables, engine
 
 
 app = FastAPI()
@@ -40,11 +22,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Create DB tables on startup
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
-
 
 # health check endpoint
 @app.get("/api/v1/health")
@@ -55,55 +32,59 @@ def health_check() -> dict:
 # targets endpoints
 @app.get("/api/v1/targets/")
 async def read_targets(
-    session: SessionDep,
     offset: int = 0,
     limit: Annotated[int, Query(le=100)] = 100
 ) -> list[Target]:
-    targets = session.exec(select(Target).offset(offset).limit(limit)).all()
+    with Session(engine) as session:
+        targets = session.exec(select(Target).offset(offset).limit(limit)).all()
     return targets
 
 
 @app.post("/api/v1/targets/")
-async def create_target(new_target: TargetCreate, session: SessionDep) -> Target:
+async def create_target(new_target: TargetCreate) -> Target:
     target = Target(name=new_target.name, url=new_target.url)
-    session.add(target)
-    session.commit()
-    session.refresh(target)
-    return target
+    with Session(engine) as session:
+        session.add(target)
+        session.commit()
+        session.refresh(target)
+        return target
 
 
 @app.get("/api/v1/targets/{target_id}")
-async def read_target(target_id: int, session: SessionDep) -> Target:
-    target = session.get(Target, target_id)
-    if not target:
-        raise HTTPException(status_code=404, detail="Target not found")
-    return target
+async def read_target(target_id: int) -> Target:
+    with Session(engine) as session:
+        target = session.get(Target, target_id)
+        if not target:
+            raise HTTPException(status_code=404, detail="Target not found")
+        return target
 
 
 @app.patch("/api/v1/targets/{target_id}")
-async def update_targets(target_id: int, new_target: TargetUpdate, session: SessionDep) -> Target:
-    target = session.get(Target, target_id)
-    if not target:
-        raise HTTPException(status_code=404, detail="Target not found")
-    print("Target:", target)
-    if new_target.name:
-        target.name = new_target.name
-    if new_target.url:
-        target.url = new_target.url
-    session.add(target)
-    session.commit()
-    session.refresh(target)
-    return target
+async def update_targets(target_id: int, new_target: TargetUpdate) -> Target:
+    with Session(engine) as session:
+        target = session.get(Target, target_id)
+        if not target:
+            raise HTTPException(status_code=404, detail="Target not found")
+        print("Target:", target)
+        if new_target.name:
+            target.name = new_target.name
+        if new_target.url:
+            target.url = new_target.url
+        session.add(target)
+        session.commit()
+        session.refresh(target)
+        return target
 
 
 @app.delete("/api/v1/targets/{target_id}")
-async def delete_target(target_id: int, session: SessionDep):
-    target = session.get(Target, target_id)
-    if not target:
-        raise HTTPException(status_code=404, detail="Target not found")
-    session.delete(target)
-    session.commit()
-    return {"ok": True}
+async def delete_target(target_id: int):
+    with Session(engine) as session:
+        target = session.get(Target, target_id)
+        if not target:
+            raise HTTPException(status_code=404, detail="Target not found")
+        session.delete(target)
+        session.commit()
+        return {"ok": True}
 
 
 # TODO: build domains endpoints
@@ -112,4 +93,5 @@ async def delete_target(target_id: int, session: SessionDep):
 # TODO: build auth endpoints
 
 if __name__ == "__main__":
+    create_db_and_tables()
     uvicorn.run(app, host="0.0.0.0", port=8000)
